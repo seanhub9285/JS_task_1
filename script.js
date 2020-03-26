8d35303c824eab7ca95283e62a369630bb82c72b
seanhub9285 <62122018+seanhub9285@users.noreply.github.com>
2020-03-26 02:10:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let my_Courses = ["Html", "CSS", "JavaScript", "PHP"];
+console.log("My name is " + my_Name);
+for(let i = 0; i < my_Courses.length; i++){
+  console.log("My courses are: " + my_Courses);
+}
+const numbers_Even = [];
+for(let i = 0; i <= 200; i++){
+  if ( i % 2 === 0 ){
+    numbers_Even.push(i);
+  }
+}
+console.log(numbers_Even);
